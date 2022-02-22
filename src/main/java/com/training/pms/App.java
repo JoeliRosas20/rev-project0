af36278d1034041448b77c1291ce09787872c435
Joel Rosas <joeli.rosas20@gmail.com>
2022-02-21 19:35:38 -0500

    Moved scanner into Bank.java and added more methods

--- src/main/java/com/training/pms/App.java
+++ src/main/java/com/training/pms/App.java
@@ -12,12 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Scanner scanner = new Scanner(System.in);
-        System.out.println( "Hello There!" );
-        System.out.println("Press 1 to log in");
-        System.out.println("Press 2 to create an account");
-        int choice = scanner.nextInt();
-        Bank.choice(choice);
+    	Bank bank = new Bank();
+    	bank.startBankApp();
     }
     
 }
--- src/main/java/com/training/pms/bank/Bank.java
+++ src/main/java/com/training/pms/bank/Bank.java
@@ -1,6 +1,19 @@
 package com.training.pms.bank;
 
+import java.util.Scanner;
+
 public class Bank {
+	
+	public void startBankApp() {
+		Scanner scanner = new Scanner(System.in);
+        System.out.println( "###################################" );
+        System.out.println("	Joel Banking App");
+        System.out.println( "###################################" );
+        System.out.println("1 Login");
+        System.out.println("2 Create Account");
+        int choice = scanner.nextInt();
+        Bank.choice(choice);
+	}
 	
 	public static void choice(int num) {
 		switch(num) {
@@ -19,6 +32,16 @@ public class Bank {
 		
 	}
 	
+	public void login() {
+		
+	}
 	
+	public void bankingAppInfo() {
+		
+	}
+	
+	public void devInfo() {
+		
+	}
 
 }
