f2162a7a259bec93cbc419eae367da85cbb8394f
Joel Rosas <joeli.rosas20@gmail.com>
2022-02-21 19:39:31 -0500

    Added the other cases

--- src/main/java/com/training/pms/bank/Bank.java
+++ src/main/java/com/training/pms/bank/Bank.java
@@ -11,6 +11,8 @@ public class Bank {
         System.out.println( "###################################" );
         System.out.println("1 Login");
         System.out.println("2 Create Account");
+        System.out.println("3 Know more about the Banking App");
+        System.out.println("4 Know more about the developers");
         int choice = scanner.nextInt();
         Bank.choice(choice);
 	}
@@ -22,6 +24,12 @@ public class Bank {
 			break;
 		case 2:
 			System.out.println("Create a new account");
+			break;
+		case 3:
+			System.out.println("Know about app");
+			break;
+		case 4:
+			System.out.println("Know about Devs");
 			break;
 		default:
 			System.out.println("Default");
