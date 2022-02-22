30f7a63eebfc9cdbc9073e3e4ad905492eaa5d58
Joel Rosas <joeli.rosas20@gmail.com>
2022-02-21 20:29:06 -0500

    Added print into the create functions

--- src/main/java/com/training/pms/bank/Bank.java
+++ src/main/java/com/training/pms/bank/Bank.java
@@ -4,17 +4,29 @@ import java.util.Scanner;
 
 public class Bank {
 	
+	static Scanner scanner = new Scanner(System.in);
+	
 	public void startBankApp() {
-		Scanner scanner = new Scanner(System.in);
-        System.out.println( "###################################" );
+		System.out.println("Entering");
+		int choice = 0;
+        Bank.message();
+        while(scanner.hasNext()) {
+        	choice = scanner.nextInt();
+        	Bank.choice(choice);
+        	//Bank.message();
+		}
+        System.out.println("Leaving");
+	}
+	
+	public static void message() {
+		System.out.println( "###################################" );
         System.out.println("	Joel Banking App");
         System.out.println( "###################################" );
         System.out.println("1 Login");
         System.out.println("2 Create Account");
         System.out.println("3 Know more about the Banking App");
         System.out.println("4 Know more about the developers");
-        int choice = scanner.nextInt();
-        Bank.choice(choice);
+        System.out.println("10.E X I T");
 	}
 	
 	public static void choice(int num) {
@@ -24,6 +36,7 @@ public class Bank {
 			break;
 		case 2:
 			System.out.println("Create a new account");
+			createAccount();
 			break;
 		case 3:
 			System.out.println("Know about app");
@@ -33,11 +46,43 @@ public class Bank {
 			break;
 		default:
 			System.out.println("Default");
+			System.exit(0);
 		}
 	}
 	
-	public void createAccount() {
-		
+	public static void createAccount() {
+		char choice = ' ';
+		System.out.println("Welcome to create/open account section");
+		System.out.println("Please enter the details to open an acccount:");
+		System.out.println("Enter your account type ( E for Employee / C for Customer ):");
+		choice = scanner.next().charAt(0);
+		if(choice != 'E' && choice != 'C') {
+			System.out.println("Incorrect account type, please enter again ");
+			System.out.println("Enter your account type ( E for Employee / C for Customer ):");
+			choice = scanner.next().charAt(0);
+		}
+		if(choice == 'E') {
+			employeeCreate();
+		}
+		else if(choice == 'C') {
+			customerCreate();
+		}
+	}
+	
+	public static void employeeCreate() {
+		System.out.println("Account details for Employee ?");
+		System.out.println("Enter employee id : ");
+		System.out.println("Enter employee name : ");
+		System.out.println("Enter your password : ");
+		System.out.println("Congrats");
+	}
+	
+	public static void customerCreate() {
+		System.out.println("Account details for Customer ?");
+		System.out.println("Enter customer id : ");
+		System.out.println("Enter customer name : ");
+		System.out.println("Enter your password : ");
+		System.out.println("Congrats");
 	}
 	
 	public void login() {
@@ -49,7 +94,7 @@ public class Bank {
 	}
 	
 	public void devInfo() {
-		
+		System.out.println("About Me:");
 	}
 
 }
