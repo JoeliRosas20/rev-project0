2f5cd539bcda3bc9503fca056b75e614f355c6c6
Joel Rosas <joeli.rosas20@gmail.com>
2022-02-22 19:57:11 -0500

    Removed the static from Bank classes

--- src/main/java/com/training/pms/dao/BankDAO.java
+++ src/main/java/com/training/pms/dao/BankDAO.java
@@ -3,7 +3,7 @@ package com.training.pms.dao;
 public interface BankDAO {
 	
 	public void approveTransaction(boolean res);
-	public void viewAccount(int accountID);
+	public void viewAccount();
 	public void viewTransactionLod(int logID);
 
 }
