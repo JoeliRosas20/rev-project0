e2fb07d9a9abb574783bedee948b89886a93b5ee
Joel Rosas <joeli.rosas20@gmail.com>
2022-02-23 21:51:37 -0500

    Added more code to LoginDAO

--- src/main/java/com/training/pms/dao/LoginDAO.java
+++ src/main/java/com/training/pms/dao/LoginDAO.java
@@ -1,8 +1,10 @@
 package com.training.pms.dao;
 
+import com.training.pms.bank.*;
+
 public interface LoginDAO {
 	
-	public boolean register();
-	public boolean validate();
+	public boolean register(Login login);
+	public boolean validate(String username, String password);
 	
 }
--- src/main/java/com/training/pms/dao/LoginDAOImpl.java
+++ src/main/java/com/training/pms/dao/LoginDAOImpl.java
@@ -1,21 +1,56 @@
 package com.training.pms.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.training.pms.bank.Login;
+import com.training.pms.utility.DBConnection;
+
 public class LoginDAOImpl implements LoginDAO{
 
-	public LoginDAOImpl() {
-		// TODO Auto-generated constructor stub
+	Connection connection = DBConnection.getConnection();
+
+	@Override
+	public boolean register(Login login) {
+		// TODO Auto-generated method stub
+		System.out.println("##Adding products :" + login);
+		PreparedStatement statement = null;
+		int rows = 0;
+		try {
+			statement = connection.prepareStatement("insert into login values(default,?,?)");
+			statement.setString(1, login.getUsername());
+			statement.setString(2, login.getPassword());
+
+			rows = statement.executeUpdate();
+			System.out.println(rows + " inserted successfully");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (rows == 0)
+			return false;
+		else
+			return true;
 	}
 
 	@Override
-	public boolean register() {
+	public boolean validate(String username, String password) {
 		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean validate() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean userValid = false;
+		PreparedStatement stat;
+		try {
+			stat = connection.prepareStatement("select * from login where username = ? and password = ?");
+			stat.setString(1, username);
+			stat.setString(2, password);
+			ResultSet res = stat.executeQuery();
+			userValid = res.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userValid;
 	}
 
 }
